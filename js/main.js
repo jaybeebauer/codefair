8b64c8b4a78c5fa45dd9a4b1295b2e9baf99a500
jaybeebauer <joshybee@gmail.com>
2017-05-19 12:23:34 +1000

    -Form validation

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Scripts for Assignment1b
+ *
+ * author: Joshua Bauer s263364
+ * email: joshybee@gmail.com
+ * website: https://jaybeebauer.github.io/jaybeestyleguide
+ *
+ */
+(function() {
+
+  $.validate({
+    modules : 'html5'
+  });
+
+})();
